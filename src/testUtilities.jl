6d6aa8159ea5ed9518f84cba2564cf719920a7d4
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-04-08 15:05:06 -0400

    Upated for refactored `match_earthchem`

--- src/testUtilities.jl
+++ src/testUtilities.jl
@@ -1,25 +1,49 @@
+using Test
+include("Utilities.jl")
+
 ## --- test match_earthchem()
-    using Test
-    include("Utilities.jl")
+    codes = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 
+        2.0, 2.1, 2.3, 
+        3.0, 3.1, 3.2
+    ]
+    testset = rand(codes, 1000);
 
-    codes = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.3, 3.1, 3.2]
-    testset = rand(codes, 100)
+    catsmj = match_earthchem(testset, major=true);
+    cats = match_earthchem(testset, major=false);
 
-    bulksed, bulkign, bulkvolc, bulkplut, bulkmet, bulkmetased, bulkmetaign = match_earthchem(testset, major=false);
-    bulksed_major, bulkign_major, bulkmet_major = match_earthchem(testset, major=true);
+    # Sedimentary counts
+    @test count(cats.alluvium) == length(findall(==(1.1), testset))
+    @test count(cats.siliciclast) == length(findall(==(1.2), testset))
+    @test count(cats.shale) == length(findall(==(1.3), testset))
+    @test count(cats.carb) == length(findall(==(1.4), testset))
+    @test count(cats.chert) == length(findall(==(1.5), testset))
+    @test count(cats.evaporite) == length(findall(==(1.6), testset))
+    @test count(cats.phosphorite) == length(findall(==(1.7), testset))
+    @test count(cats.coal) == length(findall(==(1.8), testset))
+    @test count(cats.volcaniclast) == length(findall(==(1.9), testset))
 
-    # Rocks classified correctly
-    @test (bulksed .| bulkmetased) == bulksed_major
-    @test (bulkign .| bulkvolc .| bulkplut .| bulkmetaign) == bulkign_major
-    @test bulkmet == bulkmet_major
+    # Igneous counts
+    @test count(cats.volc) == length(findall(==(3.1), testset))
+    @test count(cats.plut) == length(findall(==(3.2), testset))
 
-    # Counts done correctly
-    @test count(bulkign) == length(findall(==(3.0), testset))
-    @test count(bulkvolc) == length(findall(==(3.1), testset))
-    @test count(bulkplut) == length(findall(==(3.2), testset))
+    # Metamorphic counts
+    @test count(cats.metased) == length(findall(==(2.1), testset))
+    @test count(cats.metaign) == length(findall(==(2.3), testset))
 
-    @test count(bulkmet) == length(findall(==(2.0), testset))
-    @test count(bulkmetased) == length(findall(==(2.1), testset))
-    @test count(bulkmetaign) == length(findall(==(2.3), testset))
+    # Major classifications
+    @test cats.sed == catsmj.sed
+    @test cats.ign == catsmj.ign
+    @test cats.met == catsmj.met
+
+    @test count(cats.sed) == length(findall(==(1.0), testset)) + (count(cats.siliciclast) +
+        count(cats.shale) + count(cats.carb) + count(cats.chert) + count(cats.evaporite) + 
+        count(cats.phosphorite) + count(cats.coal) + count(cats.volcaniclast)
+    )
+    @test count(cats.ign) == length(findall(==(3.0), testset)) + (count(cats.volc) +
+        count(cats.plut)
+    )
+    @test count(cats.met) == length(findall(==(2.0), testset)) + (count(cats.metased) + 
+        count(cats.metaign)
+    )
 
 ## --- End of file

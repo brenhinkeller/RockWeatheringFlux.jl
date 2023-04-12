ebfa61aa25887e4c43585301397a5756a2682094
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-04-12 18:21:02 -0400

    Start code to match Macrostrat and Earthchem samples

---
+++ src/samplematch.jl
@@ -0,0 +1,93 @@
+## --- Match Macrostrat samples to the most likely EarthChem sample
+    # Packages
+    using MAT
+    using JLD
+    using StatGeochem
+
+    # Local utilities
+    include("Utilities.jl")
+
+
+## --- Load Earthchem bulk geochemical data
+    earthchem_raw = matopen("data/bulk.mat")
+    earthchem_dict = read(earthchem_raw, "bulk")
+    close(earthchem_raw)
+    earthchem = NamedTuple{Tuple(Symbol.(keys(earthchem_dict)))}(values(earthchem_dict))
+
+    cats_echem = match_earthchem(earthchem.Type, major=false)   # Get rock types
+
+## --- Load Earthchem metadata
+    earthchem_raw = matopen("data/bulktext.mat")
+    earthchem_dict = read(earthchem_raw, "bulktext")
+    close(earthchem_raw)
+    text_echem_unparsed = NamedTuple{Tuple(Symbol.(keys(earthchem_dict)))}(values(earthchem_dict))
+    
+
+    # Preallocate for parsing---make a NamedTuple with just the things we want
+    npoints = length(text_echem_unparsed.index["Composition"])
+
+    # Ignoring sparse arrays for now because they make me sad
+    # Correct indices for 0-index offset
+    composition_idx = Int.(text_echem_unparsed.index["Composition"] .+ 1)
+    reference_idx = Int.(text_echem_unparsed.index["Reference"] .+ 1)
+    rockname_idx = Int.(text_echem_unparsed.index["Rock_Name"] .+ 1)
+    source_idx = Int.(text_echem_unparsed.index["Source"] .+ 1)
+    type_idx = Int.(text_echem_unparsed.index["Type"] .+ 1)
+    material_idx = Int.(text_echem_unparsed.index["Material"] .+ 1)
+
+    # Parse numeric codes in index into arrays
+    composition = lowercase.(string.(text_echem_unparsed.Composition[composition_idx]))
+    reference = lowercase.(string.(text_echem_unparsed.Reference[reference_idx]))
+    rockname = lowercase.(string.(text_echem_unparsed.Rock_Name[rockname_idx]))
+    source = lowercase.(string.(text_echem_unparsed.Source[source_idx]))
+    type = lowercase.(string.(text_echem_unparsed.Type[type_idx]))
+    material = lowercase.(string.(text_echem_unparsed.Material[material_idx]))
+
+    
+## --- Load Macrostrat data
+    # Reduced size Macrostrat data file
+    retrive_file = load("data/toy_responses.jld")
+    responses = retrive_file["responses"]
+    elevations = float.(retrive_file["elevations"])
+    rocklat = float.(retrive_file["latitude"])
+    rocklon = float.(retrive_file["longitude"])    
+    npoints = retrive_file["npoints"]
+
+    # Preallocate for parsed Macrostrat data
+    rocktype = Array{String}(undef, npoints, 1)
+    rockdescrip = Array{String}(undef, npoints, 1)
+    rockname = Array{String}(undef, npoints, 1)
+    rockstratname = Array{String}(undef, npoints, 1)
+    rockcomments = Array{String}(undef, npoints, 1)
+    agemax = Array{Float64}(undef, npoints, 1)
+    agemin = Array{Float64}(undef, npoints, 1)
+
+    # Parse responses into preallocated arrays
+    for i in eachindex(rocktype)
+        rocktype[i] = get_macrostrat_lith(responses[i])
+        rockdescrip[i] = get_macrostrat_descrip(responses[i])
+        rockname[i] = get_macrostrat_name(responses[i])
+        rockstratname[i] = get_macrostrat_strat_name(responses[i])
+        rockcomments[i] = get_macrostrat_comments(responses[i])
+        agemax[i] = get_macrostrat_max_age(responses[i])
+        agemin[i] = get_macrostrat_min_age(responses[i])
+    end
+
+    # Convert strings to lowercase so they can be matched to known names of rock types
+    rocktype = lowercase.(rocktype)
+    rockdescrip = lowercase.(rockdescrip)
+    rockname = lowercase.(rockname)
+    rockstratname = lowercase.(rockstratname)
+    rockcomments = lowercase.(rockcomments)
+
+    # Replace tabs with spaces so they will not be confused with the delimitator if exported
+    rocktype = replace.(rocktype, "    " => " ")
+    rockdescrip = replace.(rockdescrip, "    " => " ")
+    rockname = replace.(rockname, "    " => " ")
+    rockstratname = replace.(rockstratname, "    " => " ")
+    rockcomments = replace.(rockcomments, "    " => " ")
+
+    # Match data to rock types
+    macro_cats = match_rocktype(rocktype, rockname, rockdescrip, major=false)
+
+### --- End of file

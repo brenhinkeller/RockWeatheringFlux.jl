966cff6225aaec19e9ff4faf4651b86fb27e71aa
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-05-03 00:01:14 -0400

    Calculate chunk indices
    Final goal is to loop through the Macrostrat data in chunks of 50 (TBD?) to (hopefully) avoid excessive memory allocations. This commit adds code that calculates the indices of those chunks

--- src/samplematch.jl
+++ src/samplematch.jl
@@ -85,6 +85,7 @@
 # TO DO: likelihood vs log likelihood? Is this already log likelihood?
 # TO DO: NaN -> for missing age and location isn't good, but some equivilent?
 # TO DO: send samples through in chunks isntead of by rock type
+	
     @info "Matching samples"
     bulk_idxs = collect(1:length(bulk.SiO2))
     matches = Dict{Symbol, Matrix{Int64}}()
@@ -92,43 +93,63 @@
     @time for type in eachindex(macro_cats)        
         # Cover is not in EarthChem data; skip it
         if type==:cover continue end
-
-		# Temporary output
-        println("$type")
-
+        
         # If going to crash: don't
         if type==:sed
             @warn "Skipping $type to avoid SigKill"
             continue
         end
-
-		# NOTE: doing this in a function is (only slightly) worse actually
-        # Get intermediate variables for the rock type we're looking at
-        bulksamples = bulk_cats[type]                      # EarthChem BitVector
-
-        lat = macrostrat.rocklat[macro_cats[type]]         # Macrostrat latitude
-        lon = macrostrat.rocklon[macro_cats[type]]         # Macrostrat longitude
-        bulklat = bulk.Latitude[bulksamples]               # EarthChem latitudes
-        bulklon = bulk.Longitude[bulksamples]              # EarthChem longitudes
-
-        sampleage = macrostrat.age[macro_cats[type]]       # Macrostrat age
-        bulkage = bulk.AgeEst[bulksamples]                 # EarthChem age
-
-        sampleidx = bulk_idxs[bulksamples]                 # Indices of EarthChem samples
-        geochemdata = geochem[type]                        # Major element compositions
         
-        # Earthchem samples for only major elements for this rock type
-        bulkgeochem = Array{Array}(undef, length(geochemdata), 1)
-        for i in 1:length(geochemkeys)
-            bulkgeochem[i] = bulk[geochemkeys[i]][bulksamples] 
-        end
-        bulkgeochem = NamedTuple{geochemkeys}(bulkgeochem)           
+        # Temporary output
+        println("$type")
+        
+        # NOTE: doing int vars in a function is (slightly) worse than this
+        # Intermediate Earthchem variables (unaffected by chunking)
+        bulksamples = bulk_cats[type]                      # EarthChem BitVector
+        bulklat = bulk.Latitude[bulksamples]               # EarthChem latitudes
+		bulklon = bulk.Longitude[bulksamples]              # EarthChem longitudes
+        bulkage = bulk.AgeEst[bulksamples]                 # EarthChem age
+	    sampleidx = bulk_idxs[bulksamples]                 # Indices of EarthChem samples
+	    
+	    geochemdata = geochem[type]                        # Major element compositions
+	    
+	    # Earthchem samples for only major elements for this rock type
+		    bulkgeochem = Array{Array}(undef, length(geochemdata), 1)
+		    for i in 1:length(geochemkeys)
+		        bulkgeochem[i] = bulk[geochemkeys[i]][bulksamples] 
+		    end
+		    bulkgeochem = NamedTuple{geochemkeys}(bulkgeochem)
+		    
+		# Get start and end coordinates for sample chunks
+		len = count(macro_cats[type]) 
+		if len != 0
+			chunks = vcat(collect(1:50:len), len)
+		else
+			continue
+		end
+        
+		for i in 1:length(chunks[1:end-1])
+			# Get chunks
+			j = chunks[i]
+        	k = chunks[i+1]-1
 
-        # Find most likely sample
-        # TO DO: pass argument as a tuple?
-        # TO DO: is there any way to do fewer than ~8M computations per sample?
-        matched_sample = likelihood(lat, lon, bulklat, bulklon, sampleidx, sampleage, bulkage, geochemdata, bulkgeochem)
-        setindex!(matches, matched_sample, type)
+		    # Get intermediate variables for the rock type we're looking at
+		    lat = macrostrat.rocklat[macro_cats[type]]         # Macrostrat latitude
+		    lon = macrostrat.rocklon[macro_cats[type]]         # Macrostrat longitude
+		    sampleage = macrostrat.age[macro_cats[type]]       # Macrostrat age
+
+		    # Find most likely sample
+		    # TO DO: pass argument as a tuple?
+		    # TO DO: is there any way to do fewer than ~several M computations per sample?
+		    # matched_sample = likelihood(lat, lon, bulklat, bulklon, sampleidx, sampleage, bulkage, geochemdata, bulkgeochem)
+		    # setindex!(matches, matched_sample, type)
+		    
+		    # Find most likely Earthchem sample in chunks of at most 100 Macrostrat samples
+        
+        	
+        	matched_sample = likelihood(lat[j:k], lon[j:k], bulklat, bulklon, sampleidx,
+        		sampleage, bulkage, geochemdata, bulkgeochem)
+        end
     end
 
 #=

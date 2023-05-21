a3a59e351ff99c6ef195dc6ac11ad1747f2e288e
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-05-21 16:13:09 -0400

    Finish wt.% and flux calculations

--- src/dev.jl
+++ src/dev.jl
@@ -1,18 +1,27 @@
 """
 
-Calculate the average wt. % and flux by source of an element.
+Calculate the average wt. % and flux (kg/yr) by source of an element.
+also returns global_flux
 
 I want this to 
     - account for the fact that not all Macrostrat samples were matched, so some indices are 0
     - Be as generic as possible, i.e. don't do subindexing for a specific element (that should
         be done outside of the function)
     - return two NTuples, one with the wt.% and one with the flux by type
+    - printout how many samples were not NaN
+    - erosion, bulk_cats, crustal_area must contain the keys :siliciclast, :shale, :carb, :chert, 
+    :evaporite, :coal, :sed,
+    :volc, :plut, :ign,
+    :metased, :metaign, :met,
+    :cryst (others ok)
 
 # Arguments
 - `crustal_density::Number=2750`: Average crustal density (kg/m³).
+- `elem::String=""`: Element being analyzed, for terminal printout
 """
 function flux_source(bulk::AbstractArray, bulkidx::Vector{Int64}, erosion::NamedTuple, 
-    crustal_area::NamedTuple; crustal_density::Number=2750)
+    bulk_cats::NamedTuple, crustal_area::NamedTuple; 
+    crustal_density::Number=2750, elem::String="")
 
     # Preallocate
     allkeys = (:siliciclast, :shale, :carb, :chert, :evaporite, :coal, :sed,
@@ -21,12 +30,38 @@ function flux_source(bulk::AbstractArray, bulkidx::Vector{Int64}, erosion::Named
         :cryst
     )
     allinitvals = fill(NaN, length(allkeys))
-    
+    npoints = length(bulkidx)
+
     wt = Dict(zip(allkeys, allinitvals))
     flux = Dict(zip(allkeys, allinitvals))
+    bulkdata = Array{Float64}(undef, npoints, 1)
+    datacount = 0
 
     # Get EarthChem samples
+    for i in eachindex(bulkidx)
+        notzero = bulkidx[i] != 0
+        if notzero
+            bulkdata[i] = bulk[bulkidx[i]]
+            datacount += 1
+        else
+            bulkdata[i] = NaN
+        end
+    end
 
+    @info "$datacount of $npoints $elem samples ($(round(Int, datacount/npoints))%) are not NaN"
 
-    return wt, flux
+    # Calculate average wt.% for each rock type
+    for i in eachindex(allkeys)
+        wt[allkeys[i]] = nanmean(bulkdata[bulk_cats[i]])
+    end
+    wt = NamedTuple{Tuple(allkeys)}(values(wt))
+
+    # Calculate provenance by rock type
+    for i in eachindex(allkeys)
+        flux[allkeys[i]] = erosion[i] * wt[i] * crustal_area[i] * crustal_density * 1e-6
+    end
+    flux = NamedTuple{Tuple(allkeys)}(values(flux))
+    global_flux = flux.sed + flux.ign + flux.met
+
+    return wt, flux, global_flux
 end

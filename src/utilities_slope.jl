440e52d5120bb4e93b0b5d17aba631ff049e6049
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-06-18 14:29:14 -0400

    Move slope-related utilities to dedicated file
    Functions that are only used for creating the slope model will go in utilities_slope, which has been renamed from OCTOPUS_utilities.jl

--- src/utilities_slope.jl
+++ src/utilities_slope.jl
@@ -204,26 +204,145 @@
     end
 
 ## --- Calculate precipitation
-"""
-```julia
-find_precip(lat,lon)
-```
-for each basin? or each coordinate?
-"""
-function find_precip(lat,lon)
-    prate = ncread("data/prate.sfc.mon.ltm.nc","prate")
-    precip = mean(prate, dims=3)[:,:,1]
+    """
+    ```julia
+    find_precip(lat,lon)
+    ```
+    for each basin? or each coordinate?
+    """
+    function find_precip(lat,lon)
+        prate = ncread("data/prate.sfc.mon.ltm.nc","prate")
+        precip = mean(prate, dims=3)[:,:,1]
 
-    out = Array{Float64}(undef,size(lat))
-    for i=1:length(lat)
-        if isnan(lat[i]) || isnan(lon[i]) || lat[i] > 88.542 || lat[i] < -88.542
-            out[i] = NaN
-        else
-            row = trunc(Int, mod(lon[i]*192/360 + 0.5, 192.0)) + 1
-            col = trunc(Int, 46.99999 - lat[i]*94/177.084) + 1
-            out[i] = precip[row, col]
+        out = Array{Float64}(undef,size(lat))
+        for i=1:length(lat)
+            if isnan(lat[i]) || isnan(lon[i]) || lat[i] > 88.542 || lat[i] < -88.542
+                out[i] = NaN
+            else
+                row = trunc(Int, mod(lon[i]*192/360 + 0.5, 192.0)) + 1
+                col = trunc(Int, 46.99999 - lat[i]*94/177.084) + 1
+                out[i] = precip[row, col]
+            end
         end
+
+        return out
     end
 
-    return out
-end
+
+## --- Functions for dealing with SRTM15
+    resourcepath = "data"
+
+    # Read srtm15plus file from HDF5 storage, downloading from cloud if necessary
+    function get_srtm15plus_aveslope(varname="")
+        # Available variable names: "slope", "y_lat_cntr", "x_lon_cntr",
+        # "nanval", "cellsize", "scalefactor", and "reference". Units are
+        # meters of elevation and decimal degrees of latitude and longitude
+
+        # Construct file path
+        filepath = joinpath(resourcepath,"srtm15plus_aveslope.h5")
+
+        # Download HDF5 file from Google Cloud if necessary
+        if ~isfile(filepath)
+            print("Downloading srtm15plus.h5 from google cloud storage\n")
+            download("https://storage.googleapis.com/statgeochem/srtm15plus_aveslope.h5", filepath)
+        end
+
+        # Read and return the file
+        return h5read(filepath, "vars/"*varname)
+    end
+
+    # Find the elevation of points at position (lat,lon) on the surface of the
+    # Earth, using the SRTM15plus 15-arc-second elevation model.
+    function find_srtm15plus_aveslope(srtm15plus,lat,lon)
+
+        # Interpret user input
+        if length(lat) != length(lon)
+            error("lat and lon must be equal length\n")
+        elseif isa(srtm15plus,Dict)
+            data = srtm15plus["slope"]
+        elseif isa(srtm15plus, Array)
+            data = srtm15plus
+        else
+            error("wrong srtm15plus variable")
+        end
+
+        # Scale factor (cells per degree) = 60 * 4 = 240
+        # (15 arc seconds goes into 1 arc degree 240 times)
+        sf = 240
+
+        # Create and fill output vector
+        out=Array{Float64}(size(lat));
+        for i=1:length(lat)
+            if isnan(lat[i]) || isnan(lon[i]) || lat[i]>90 || lat[i]<-90 || lon[i]>180 || lon[i]<-180
+                # Result is NaN if either input is NaN or out of bounds
+                out[i] = NaN
+            else
+                # Convert latitude and longitude into indicies of the elevation map array
+                # Note that STRTM15 plus has N+1 columns where N = 360*sf
+                row = 1 + round(Int,(90+lat[i])*sf)
+                col = 1 + round(Int,(180+lon[i])*sf)
+                # Find result by indexing
+                res = data[row,col]
+                if res > 1000
+                    out[i] = NaN
+                else
+                    out[i] = res
+                end
+            end
+        end
+
+        return out
+    end
+
+    function find_srtm15plus_aveslope_around(srtm15plus,lat,lon; halfwidth=1::Integer, max_allowed_slope=1000::Number)
+
+        # Interpret user input
+        if length(lat) != length(lon)
+            error("lat and lon must be equal length\n")
+        elseif isa(srtm15plus,Dict)
+            data = srtm15plus["slope"]
+        elseif isa(srtm15plus, Array)
+            data = srtm15plus
+        else
+            error("wrong srtm15plus variable")
+        end
+
+        # Scale factor (cells per degree) = 60 * 4 = 240
+        # (15 arc seconds goes into 1 arc degree 240 times)
+        sf = 240
+
+        # Create and fill output vector
+        out=Array{Float64}(size(lat));
+        for i=1:length(lat)
+            if isnan(lat[i]) || isnan(lon[i]) || lat[i]>90 || lat[i]<-90 || lon[i]>180 || lon[i]<-180
+                # Result is NaN if either input is NaN or out of bounds
+                out[i] = NaN
+            else
+                # Convert latitude and longitude into indicies of the elevation map array
+                # Note that STRTM15 plus has N+1 columns where N = 360*sf
+                row = 1 + round(Int,(90+lat[i])*sf)
+                col = 1 + round(Int,(180+lon[i])*sf)
+
+                # Find result by indexing
+                k = 0;
+                out[i] = 0;
+                for r = (row-halfwidth):(row+halfwidth)
+                    for c = (col-halfwidth):(col+halfwidth)
+                        if r>0 && r<43202
+                            res = data[r,mod(c-1,86400)+1]
+                            if res < max_allowed_slope
+                                k +=1
+                                out[i] += res
+                            end
+                        end
+                    end
+                end
+                out[i] /= k
+            end
+        end
+
+        return out
+    end
+
+
+## -- End of file

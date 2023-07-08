6e72a2c12001555bb6ed1a8ab49e40c55a043794
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-07-08 17:37:54 -0400

    Restrict bulk to 84-104% analyzed wt.
    Removed old code that only re-did units. Writes new file with only the samples and elements we'll want to analyze

--- src/ScreenBulk.jl
+++ src/ScreenBulk.jl
@@ -24,6 +24,7 @@
     # Packages
     using MAT
     using StatGeochem
+    using LoopVectorization
 
     # Local utilities
     include("Utilities.jl")
@@ -49,7 +50,7 @@
     end
 
 ## --- Define and organize elements to correct
-    majors = [:SiO2,:Al2O3,:Fe2O3T,:TiO2,:MgO,:CaO,:Na2O,:K2O,]
+    majors = [:SiO2,:Al2O3,:FeOT,:TiO2,:MgO,:CaO,:Na2O,:K2O,]
     minors = [:Ag,:As,:Au,:B,:Ba,:Be,:Bi,:C,:CaCO3,:Cd,:Ce,:Cl,:Co,:Cr2O3,:Cs,:Cu,:Dy,:Er,
         :Eu,:F,:Ga,:Gd,:Hf,:Hg,:Ho,:I,:In,:Ir,:La,:Li,:Lu,:MnO,:Mo,:Nb,:Nd,:NiO,:Os,
         :P2O5,:Pb,:Pd,:Pt,:Pr,:Re,:Rb,:Sb,:Sc,:Se,:S,:Sm,:Sn,:Sr,:Ta,:Tb,:Te,:Th,:Tl,:Tm,
@@ -57,14 +58,16 @@
     ]
 
     # Collect major and minor elements together
-    allconstit = vcat(majors, minors)
+    allconstit = [majors; minors]
     ndata = length(allconstit)
 
     # Get all units present in bulktext
     presentunits = join(collect(keys(bulktext.unit)), " ")
 
     # Define densities for relevant major element oxides (g/cm³)
-    density = (Al2O3=3.95,K2O=2.35,MgO=3.58,Na2O=2.27,P2O5=2.39,SiO2=2.65,TiO2=4.23, CaO=3.34)
+    density = (Al2O3=3.95, K2O=2.35, MgO=3.58, Na2O=2.27, P2O5=2.39, SiO2=2.65, TiO2=4.23, 
+        CaO=3.34, FeOT=5.74
+    )
 
 
 ## --- Convert all units to wt.%
@@ -80,73 +83,26 @@
     end
 
 
-## --- Check how many samples have total geochemical composition
-    # Calculate total wt.% represented by major elements
-    allmajors = Array{Float64}(undef, length(bulk.SiO2), 1)
-    for i in eachindex(allmajors)
-        for j in majors
-            allmajors[i] = nanadd(allmajors[i], bulk[j][i])
+## --- Restrict data to 84-104 wt.% analyzed
+    bulkweight = zeros(length(bulk.SiO2));
+    @time @turbo for i in eachindex(allconstit)
+        for j in eachindex(bulkweight)
+            bulkweight[j] = nanadd(bulkweight[j], bulk[allconstit[i]][j])
         end
     end
 
-    # Calculate percentage of samples with wt.% of major elements oxides above...
-    total = length(bulk.SiO2)
-    t95 = length(findall(>(95), allmajors))     # Above 95%
-    t90 = length(findall(>(90), allmajors))     # Above 90%
-    t75 = length(findall(>(75), allmajors))     # Above 75%
-    t50 = length(findall(>(50), allmajors))     # Above 50%
+    # Restrict data to within bounds and write to file
+    bounds = (84, 104)
+    t = @. bounds[1] < bulkweight < bounds[2]
 
-    @info """
-    Percentage of samples with total major element oxide wt.% above...
-    95%: $(round(t95/total*100))
-    90%: $(round(t90/total*100))
-    75%: $(round(t75/total*100))
-    50%: $(round(t50/total*100))
-    <50%: $(round((total-t50)/total*100))
-    """
+    nsamples = round(count(t)/length(t)*100, digits=2)
+    @info "Saving $nsamples% samples between $(bounds[1])% and $(bounds[2])% analyzed wt."
 
-    # Check for all major and trace elements
-    allelems = Array{Float64}(undef, length(bulk.SiO2), 1)
-    for i in eachindex(allelems)
-        for j in allconstit
-            allelems[i] = nanadd(allelems[i], bulk[j][i])
-        end
+    bulknew = Dict()
+    for i in allconstit
+        bulknew[string(i)] = bulk[i][t]
     end
-
-    # Calculate percentage of samples with wt.% of all elements oxides above...
-    total = length(bulk.SiO2)
-    t95 = length(findall(>(95), allelems))     # Above 95%
-    t90 = length(findall(>(90), allelems))     # Above 90%
-    t75 = length(findall(>(75), allelems))     # Above 75%
-    t50 = length(findall(>(50), allelems))     # Above 50%
-
-    @info """
-    Percentage of samples with total wt.% above...
-    95%: $(round(t95/total*100))
-    90%: $(round(t90/total*100))
-    75%: $(round(t75/total*100))
-    50%: $(round(t50/total*100))
-    <50%: $(round((total-t50)/total*100))
-    """
-
-    # Output of above:
-    # ┌ Info: Percentage of samples with total major element oxide wt.% above...
-    # │        95%: 38.0
-    # │        90%: 43.0
-    # │        75%: 46.0
-    # │        50%: 48.0
-    # └        <50%: 52.0
-    # ┌ Info: Percentage of samples with total wt.% above...
-    # │        95%: 41.0
-    # │        90%: 44.0
-    # │        75%: 48.0
-    # │        50%: 51.0
-    # └        <50%: 49.0
-
-    
-## --- Write corrected data to a new file
-    bulkdict = Dict(zip(string.(keys(bulk)), values(bulk)))
-    matwrite("data/bulk_newunits.mat", bulkdict)
+    matwrite("data/bulknew.mat", bulknew)
 
 
 ## --- End of File

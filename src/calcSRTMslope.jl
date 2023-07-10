c6a1fda14be647f79397892c9cafe376f989160c
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-07-10 00:56:32 -0400

    Update SRTM15+ file location
    load from \data instead of \resources

--- src/calcSRTMslope.jl
+++ src/calcSRTMslope.jl
@@ -6,7 +6,13 @@
 
     # Get SRTM15+ file
     @info "Loading SRTM"
-    srtm = get_srtm15plus()
+    try
+        srtm = h5read("data/srtm15plus.h5", "vars/")
+    catch
+        # Note that this will download the SRTM15+ file to \resources, not \data
+        srtm = get_srtm15plus()
+    end
+
 
 ## --- Calculate maximum slope and save data set
     @info "Calculating slope. This may take up to 30 minutes. Started $(Dates.format(now(), "HH:MM"))"
@@ -15,7 +21,7 @@
     # Save results to the data folder
     @info "Saving slope to HDF5 file"
     filename = "srtm15plus_maxslope"
-    fid = h5open("data/$filename.h5","w")
+    fid = h5open("output/$filename.h5","w")
     g = create_group(fid, "vars")
 
     # Copy over SRTM15+ location data
@@ -23,6 +29,7 @@
     g["x_lon_cntr"] = srtm["x_lon_cntr"]
     g["cellsize"] = srtm["cellsize"]
     g["scalefactor"] = srtm["scalefactor"]
+    close(srtm)
 
     # Add a data set for slope and compress data (Takes about 2.5 minutes)
     @time g["slope", compress=3] = slope

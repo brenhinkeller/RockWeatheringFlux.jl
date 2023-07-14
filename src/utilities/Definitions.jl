bdef4bc968bef5d551d6202d7742670dfec40193
RowanGregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-07-13 23:23:33 -0700

    fix typo in file name

--- src/utilities/Definitions.jl
+++ src/utilities/Definitions.jl
@@ -10,7 +10,7 @@
 
     # 250,000 Macrostrat samples
         # macrostrat_io = "output/responses250000.tsv"
-        macrostrat_io = "output/resposnes250k.h5"
+        macrostrat_io = "output/responses250k.h5"
         matchedbulk_io = "output/bulkidx250k.tsv"
 
         ucc_out = "results/exposedcrust250k.tsv"

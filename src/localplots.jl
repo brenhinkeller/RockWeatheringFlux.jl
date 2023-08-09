ae83d170f6b2cb3bd54f2e6ff8072e25dcaaf81c
RowanGregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-08-08 21:14:13 -0700

    File organization and functions

--- src/localplots.jl
+++ src/localplots.jl
@@ -1,23 +1,26 @@
 ## --- Set up
-    # Computational Packages
+    # Base computational packages
     using StatGeochem
     using HDF5
     using LoopVectorization
     using Measurements
     using Static
-    using MAT
-
-    # Plotting Packages
-    using CairoMakie
-    using GeoMakie
-    using ImageMagick
+    using DelimitedFiles
+    using ProgressMeter
 
     # Local utilities
     include("utilities/Utilities.jl")
 
     colorsch = :jet1
 
-## --- Plot bulk data from wt.% restricted
+## --- Location and age of EarthChem (bulk) points
+    # Packages
+    using MAT
+    using CairoMakie
+    using GeoMakie
+    using ImageMagick
+
+    # From wt.% restricted to 84-104 total wt.% 
     bulkfid = h5open("output/bulk.h5", "r")
         header = read(bulkfid["bulk"]["header"])
         data = read(bulkfid["bulk"]["data"])
@@ -33,8 +36,7 @@
     display(f)
     save("bulk_restricted.png", f)
 
-
-## --- Plot unrestricted bulk data
+    # All data
     bulk = matread("data/bulk.mat")["bulk"]
     bulk = NamedTuple{Tuple(Symbol.(keys(bulk)))}(values(bulk))
 
@@ -47,18 +49,24 @@
     display(f)
     save("bulk_all.png", f)
 
-## --- Plot matched bulk points
-    # Matched samples
+## --- Location and age of bulk points matched to Macrostrat samples
+    # Packages
+    using CairoMakie
+    using GeoMakie
+    using ImageMagick
+
+    # Get indices of matched samples
     bulkidx = Int.(vec(readdlm("$matchedbulk_io")))
     t = @. bulkidx != 0
 
-    # Earthchem
+    # Get matched Earthchem (bulk) samples
     bulkfid = h5open("output/bulk.h5", "r")
     header = read(bulkfid["bulk"]["header"])
     data = read(bulkfid["bulk"]["data"])
     bulk = NamedTuple{Tuple(Symbol.(header))}([data[:,i][bulkidx[t]] for i in eachindex(header)])
     close(bulkfid)
 
+    # Plot
     f = Figure(resolution = (1200, 600))
     ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
     h = CairoMakie.scatter!(ax, vec(bulk.Longitude), vec(bulk.Latitude), color=vec(bulk.Age),
@@ -68,12 +76,18 @@
     display(f)
     save("bulk_matched.png", f)
 
-## --- Plot Macrostrat samples
-    # Matched samples
+
+## --- Location and age of Macrostrat samples
+    # Packages
+    using CairoMakie
+    using GeoMakie
+    using ImageMagick
+
+    # Get indices of matched samples
     bulkidx = Int.(vec(readdlm("$matchedbulk_io")))
     t = @. bulkidx != 0
 
-    # Macrostrat
+    # Get Macrostrat samples with a known match
     macrofid = h5open("$macrostrat_io", "r")
     macrostrat = (
         rocklat = read(macrofid["rocklat"])[t],
@@ -82,6 +96,7 @@
     )
     close(macrofid)
 
+    # Plot
     f = Figure(resolution = (1200, 600))
     ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
     h = CairoMakie.scatter!(ax, macrostrat.rocklon, macrostrat.rocklat, 
@@ -92,78 +107,66 @@
     save("macrostrat.png", f)
 
 
-## --- Visualize distance between matched points and Macrostrat data
-    # Location data
-    data = importdataset("dist.csv", ',', importas=:Tuple)
-    macrostrat = (rocklat = data.lat_m, rocklon = data.lon_m)
-    dist = data.dist
+## --- [DATA MISSING] Difference in SiO₂ and location between bulk and Macrostrat
+    # # Location data
+    # data = importdataset("dist.csv", ',', importas=:Tuple)
+    # macrostrat = (rocklat = data.lat_m, rocklon = data.lon_m)
+    # dist = data.dist
 
-    # silica data
-    silica = importdataset("silica.csv", ',', importas=:Tuple)
+    # # silica data
+    # silica = importdataset("silica.csv", ',', importas=:Tuple)
 
-    # Distance between sample and matched point
-    f = Figure(resolution = (1200, 600))
-    ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
-    h = CairoMakie.scatter!(ax, macrostrat.rocklon, macrostrat.rocklat, color=dist,
-        markersize = 3, alpha=0.5
-    )
-    Colorbar(f[1,2], h, label = "Arc Degrees from Matched Point", height = Relative(0.9))
-    display(f)
+    # # Distance between sample and matched point
+    # f = Figure(resolution = (1200, 600))
+    # ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
+    # h = CairoMakie.scatter!(ax, macrostrat.rocklon, macrostrat.rocklat, color=dist,
+    #     markersize = 3, alpha=0.5
+    # )
+    # Colorbar(f[1,2], h, label = "Arc Degrees from Matched Point", height = Relative(0.9))
+    # display(f)
 
-    # Silica content of matched samples
-    t = silica.SiO2 .> 50
+    # # Silica content of matched samples
+    # t = silica.SiO2 .> 50
 
-    f = Figure(resolution = (1200, 600))
-    ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
-    h = CairoMakie.scatter!(ax, silica.lon[t], silica.lat[t], color=silica.SiO2[t],
-        markersize = 3, alpha=0.5
-    )
-    Colorbar(f[1,2], h, label = "SiO₂ [wt/%]", height = Relative(0.9))
-    display(f)
+    # f = Figure(resolution = (1200, 600))
+    # ax = GeoAxis(f[1,1]; coastlines = true, dest = "+proj=wintri")
+    # h = CairoMakie.scatter!(ax, silica.lon[t], silica.lat[t], color=silica.SiO2[t],
+    #     markersize = 3, alpha=0.5
+    # )
+    # Colorbar(f[1,2], h, label = "SiO₂ [wt/%]", height = Relative(0.9))
+    # display(f)
 
 
-## --- Plot histograms of silica content by igneous rock type
-    # Run UCC calculations
-    using Plots
-    using StatGeochem
-    using HDF5
-    using LoopVectorization
-    using Measurements
-    using Static
-    using DelimitedFiles
-    using ProgressMeter
-    include("utilities/Utilities.jl")
-    
-    # Matched samples
+## --- [DATA] SiO₂ content by igneous rock type
+    # Get indicies of matched samples
     bulkidx = Int.(vec(readdlm("$matchedbulk_io")))
     t = @. bulkidx != 0
 
-    # Macrostrat
+    # Matched Macrostrat samples
     macrofid = h5open("$macrostrat_io", "r")
     macrostrat = (
-        rocktype = read(macrofid["rocktype"])[t],
-        rockname = read(macrofid["rockname"])[t],
-        rockdescrip = read(macrofid["rockdescrip"])[t]
+        # rocktype = read(macrofid["rocktype"])[t],
+        # rockname = read(macrofid["rockname"])[t],
+        # rockdescrip = read(macrofid["rockdescrip"])[t]
+        age = read(macrofid["age"])[t],
+        type = read(macrofid["typecategory"])[t]
     )
     close(macrofid)
-    macro_cats = match_rocktype(macrostrat.rocktype, macrostrat.rockname, 
-        macrostrat.rockdescrip
-    )
+    # macro_cats = match_rocktype(macrostrat.rocktype, macrostrat.rockname, 
+    #     macrostrat.rockdescrip
+    # )
+    macro_cats = match_rocktype(macrostrat.type)
 
-    # Earthchem
+    # Earthchem for each matched sample
     bulkfid = h5open("output/bulk.h5", "r")
     header = read(bulkfid["bulk"]["header"])
     data = read(bulkfid["bulk"]["data"])
-    bulk = NamedTuple{Tuple(Symbol.(header))}([data[:,i][bulkidx[t]] for i in eachindex(header)])
     close(bulkfid)
 
-    # Resampled EarthChem
-    rsign = importdataset("output/bulk_ignsilica_rs.tsv", '\t', importas=:Tuple)
-    rsvolc = importdataset("output/bulk_volcsilica_rs.tsv", '\t', importas=:Tuple)
-    rsplut = importdataset("output/bulk_plutsilica_rs.tsv", '\t', importas=:Tuple)
+    bulk = NamedTuple{Tuple(Symbol.(header))}([data[:,i][bulkidx[t]] for i in eachindex(header)])
 
 
-## --- Sample data plots; normalizing distributions
+## --- SiO₂ distribution by igneous rock type
     # All igneous
     c, n = bincounts(bulk.SiO2[macro_cats.ign], 40, 80, 40)
     n = float(n) ./ nansum(float(n) .* step(c))
@@ -192,68 +195,102 @@
     savefig("c_plut.png")
 
 
-## --- Resampled data plots; normalizing distributions
+## --- [DATA] Resampled EarthChem SiO₂ (spatial density only)
+    # Igneous
+    rs_ign = importdataset("output/bulk_ignsilica_rs.tsv", '\t', importas=:Tuple)
+    rs_volc = importdataset("output/bulk_volcsilica_rs.tsv", '\t', importas=:Tuple)
+    rs_plut = importdataset("output/bulk_plutsilica_rs.tsv", '\t', importas=:Tuple)
+
+
+## --- Resampled SiO₂ distribution by igneous rock type
     # All igneous
-    c, n, = bincounts(rsign.SiO2, 40, 80, 160)
+    c, n, = bincounts(rs_ign.SiO2, 40, 80, 160)
     n = float(n) ./ nansum(float(n) .* step(c))
-    h = plot(c, n, seriestype=:bar, label="All Igneous (resample); n = $(length(rsign.SiO2))", 
+    h = plot(c, n, seriestype=:bar, label="All Igneous (resample); n = $(length(rs_ign.SiO2))", 
         ylabel="Weight", xlabel="SiO2 [wt.%]", framestyle=:box, color=:purple, 
         linecolor=:purple, ylims=(0, round(maximum(n), digits=2)+0.01))
     display(h)
-    savefig("c_rsign.png")
+    savefig("c_rs_ign.png")
 
     # Volcanic
-    c, n, = bincounts(rsvolc.SiO2, 40, 80, 160)
+    c, n, = bincounts(rs_volc.SiO2, 40, 80, 160)
     n = float(n) ./ nansum(float(n) .* step(c))
-    h = plot(c, n, seriestype=:bar, label="Volcanic (resample); n = $(length(rsvolc.SiO2))", 
+    h = plot(c, n, seriestype=:bar, label="Volcanic (resample); n = $(length(rs_volc.SiO2))", 
         ylabel="Weight", xlabel="SiO2 [wt.%]", framestyle=:box, color=:purple, 
         linecolor=:purple, ylims=(0, round(maximum(n), digits=2)+0.01))
     display(h)
-    savefig("c_rsvolc.png")
+    savefig("c_rs_volc.png")
 
     # Plutonic
-    c, n, = bincounts(rsplut.SiO2, 40, 80, 160)
+    c, n, = bincounts(rs_plut.SiO2, 40, 80, 160)
     n = float(n) ./ nansum(float(n) .* step(c))
-    h = plot(c, n, seriestype=:bar, label="Plutonic (resample); n = $(length(rsplut.SiO2))", 
+    h = plot(c, n, seriestype=:bar, label="Plutonic (resample); n = $(length(rs_plut.SiO2))", 
         ylabel="Weight", xlabel="SiO2 [wt.%]", framestyle=:box, color=:purple, 
         linecolor=:purple, ylims=(0, round(maximum(n), digits=2)+0.01))
     display(h)
-    savefig("c_rsplut.png")
+    savefig("c_rs_plut.png")
 
 
-## --- Check how many indices in a given bin are from one sample
-    # Given an index i and bin centers c...
-    # c, n, = bincounts(rsvolc.SiO2, 40, 80, 160)
-    c, n, = bincounts(rsplut.SiO2, 40, 80, 160)
-    # c, n, = bincounts(rsign.SiO2, 40, 80, 160)
+## --- [FUNCTION] Find the most common EarthChem sample in the modal bin
+    using Plots
 
-    filter = macro_cats.plut
-    i = findmax(n)[2]
+    """
+    ```julia
+    sameindex(type::Symbol, macro_cats, bulk, bulkidx)
+    ```
 
-    s = step(c)/2
-    s = @. c[i]-s <= bulk.SiO2[filter] <= c[i]+s
+    Count how many samples in the modal bin are from the same EarthChem sample.
+    """
+    function sameindex(type::Symbol, macro_cats, bulk, bulkidx)
+        filter = macro_cats[type]
 
-    ind = bulkidx[t][filter][s]
-    unind = unique(ind)
-    counts = [count(==(i), ind) for i in unind]
+        c, n, = bincounts(bulk.SiO2[filter], 40, 80, 160)
 
-    f = findmax(counts)[1] / length(ind)
-    i = findmax(counts)[2]
-    @info "$(round(f*100, digits=2))% of indices are from EarthChem sample i = $(unind[i])"
+        # Find modal bin and all the points in it
+        i = findmax(n)[2]
+        s = step(c)/2
+        s = @. c[i]-s <= bulk.SiO2[filter] <= c[i]+s
+
+        # Find indices of those points, and count the frequency
+        ind = bulkidx[filter][s]
+        unind = unique(ind)
+        counts = [count(==(i), ind) for i in unind]
+
+        # What percent of the indices are the mode?
+        f = findmax(counts)[1] / length(ind)
+        i = findmax(counts)[2]
+
+        # Terminal printout
+        @info "$(round(f*100, digits=2))% of indices are from EarthChem sample i = $(unind[i])"
+
+        # Histogram
+        c = sort!(unique(bulkidx[filter]))
+        n = [count(==(i), bulkidx[filter]) for i in c]
+        h = plot(c, n, seriestype=:bar, label="$type", xlabel="Index [all bins]", 
+            ylabel="Frequency", framestyle=:box, ylims=(0, maximum(n)+100), color=:black
+        )
+        display(h)
+    end
 
 
-## --- Take a look at the matched sample
+## --- [FN CALL] sameindex() modal bin index counter
+    # Igneous
+    sameindex(:ign, macro_cats, bulk, bulkidx[t])        # All igneous
+    sameindex(:volc, macro_cats, bulk, bulkidx[t])       # Plutonic 
+    sameindex(:plut, macro_cats, bulk, bulkidx[t])       # Volcanic
+
+
+## --- Get EarthChem (meta)data for a given sample index
     # Set sample value
     i = 413791
 
-    # Bulk cannot be restricted
+    # Load EarthChem data
     bulkfid = h5open("output/bulk.h5", "r")
     header = read(bulkfid["bulk"]["header"])
     data = read(bulkfid["bulk"]["data"])
     bulk = NamedTuple{Tuple(Symbol.(header))}([data[:,i] for i in eachindex(header)])
-    # bulk_cats = match_earthchem(bulk.Type, major=false)
 
-    # Bulk rock name, type, and material
+    # Load EarthChem metadata
     path = bulkfid["bulktext"]["sampledata"]
     header = read(path["header"])
     index = read(path["index"])
@@ -277,4 +314,6 @@
 
     # Query Macrostrat at that location:
     # https://macrostrat.org/api/mobile/map_query?lat=LAT&lng=LON&z=11
+
+    
 ## --- End of File

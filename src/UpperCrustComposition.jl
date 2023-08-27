83fca1bc332f74d98052c35328df097716944f52
RowanGregoire <rowan.k.gregoire.23@dartmouth.edu>
2023-08-27 15:24:21 -0700

    Update Macrostrat/EarthChem file structures

--- src/UpperCrustComposition.jl
+++ src/UpperCrustComposition.jl
@@ -23,12 +23,25 @@
     # Macrostrat
     macrofid = h5open("$macrostrat_io", "r")
     macrostrat = (
-        rocklat = read(macrofid["rocklat"])[t],
-        rocklon = read(macrofid["rocklon"])[t],
-        age = read(macrofid["age"])[t],
-        type = read(macrofid["type"])[t]
+        rocklat = read(macrofid["vars"]["rocklat"])[t],
+        rocklon = read(macrofid["vars"]["rocklon"])[t],
+        age = read(macrofid["vars"]["age"])[t],
     )
-    macro_cats = match_rocktype(macrostrat.type)
+    header = read(macrofid["type"]["macro_cats_head"])
+    data = read(macrofid["type"]["macro_cats"])
+    data = @. data > 0
+    macro_cats = NamedTuple{Tuple(Symbol.(header))}([data[:,i][t] for i in eachindex(header)])
+
+    # Major types include minor types
+    for type in minorsed
+        macro_cats.sed .|= macro_cats[type]
+    end
+    for type in minorign
+        macro_cats.ign .|= macro_cats[type]
+    end
+    for type in minormet
+        macro_cats.met .|= macro_cats[type]
+    end
     close(macrofid)
 
     # Earthchem
@@ -75,57 +88,6 @@
     rows = string.(allelements)
     cols = hcat("", string.(reshape(collect(keys(UCC)), (1, length(keys(UCC))))))
     writedlm("$ucc_out", vcat(cols, hcat(rows, results)))
-
-    
-## --- Canadian Shield Experiment
-    # Outline of shield, get points inside
-    # cns = importdataset("data/shield.csv", ',', importas=:Tuple)
-    # lats, lons, inshield = coords_in_shape(cns.lon, cns.lat, macrostrat.rocklon, macrostrat.rocklat)
-
-    
-## --- Daly Gap problems
-    # # Plutonic
-    # count(macro_cats.plut)
-    # count(macro_cats.plut .& ign_cats.maf)
-    # count(macro_cats.plut .& ign_cats.int)
-    # count(macro_cats.plut .& ign_cats.fel)
-
-    # # Volcanic
-    # count(macro_cats.volc)
-    # count(macro_cats.volc .& ign_cats.maf)
-    # count(macro_cats.volc .& ign_cats.int)
-    # count(macro_cats.volc .& ign_cats.fel)
-
-    # # Uncategorized
-    # count(macro_cats.ign .& .!(macro_cats.plut .| macro_cats.volc))
-    # count(macro_cats.ign .& .!(macro_cats.plut .| macro_cats.volc) .& ign_cats.maf)
-    # count(macro_cats.ign .& .!(macro_cats.plut .| macro_cats.volc) .& ign_cats.int)
-    # count(macro_cats.ign .& .!(macro_cats.plut .| macro_cats.volc) .& ign_cats.fel)
-
-    # # All
-    # count(macro_cats.ign)
-    # count(ign_cats.maf)
-    # count(ign_cats.int)
-    # count(ign_cats.fel)
-
-
-## --- More experimentation to see what's going on with igneous rocks
-    # allnames = [get_type(name_cats, i, all_keys=true) for i in eachindex(macro_cats.sed)]
-    # ignnames = unique(allnames[macro_cats.ign])
-
-    # class = Array{String}(undef, length(rocknames))
-    # silica = Array{Float64}(undef, length(rocknames))
-    # silica_err = Array{Float64}(undef, length(rocknames))
-
-    # for i in eachindex(rocknames)
-    #     class[i] = string(class_up(typelist, rocknames[i]))
-    #     silica[i] = bulk_lookup[Symbol(rocknames[i])].SiO2.m 
-    #     silica_err[i] = bulk_lookup[Symbol(rocknames[i])].SiO2.e
-    # end
-
-    # writedlm("rockname_silica.csv", vcat(["name" "class" "silica" "1 sigma"], 
-    #     hcat(collect(rocknames), class, silica, silica_err)), ','
-    # )
 
 
 ## --- End of file

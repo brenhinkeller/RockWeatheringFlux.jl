c9c277b0a38d03ebfad1826322ecd616eb08762c
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2024-01-29 12:49:14 -0500

    More distribution breakdowns

--- src/visualization/chapter1_composition/SilicaDistribution.jl
+++ src/visualization/chapter1_composition/SilicaDistribution.jl
@@ -6,7 +6,7 @@
     using KernelDensity, MAT 
 
     # Load data and base packages
-    include("Definitions.jl")
+    include("Definitions.jl");
 
     # Filter NaNs from my samples 
     here = @. !isnan(mbulk.SiO2);
@@ -272,10 +272,208 @@
             fig[i] = h
         end
         ncols = ceil(Int, length(d)/3)
-        h = Plots.plot(fig..., layout = (3,ncols), size=(600*ncols, 400*3))
+        h = Plots.plot(fig..., layout = (3,ncols), size=(600*ncols, 400*3), 
+            title="Matched Samples [$geochem_fid]")
         display(h)
     end
 
-    
+
+## --- Does the same trend happen in the unmatched data?
+    minorsed, minorvolc, minorplut, minorign = get_rock_class()[2:5];
+    mplut = (:plut, minorplut...)   # 10 (3x4)
+    mvolc = (:volc, minorvolc...)   # 8  (3x3)
+    exclude_minor!(bulk_cats)
+    here = @. !isnan(bulk.SiO2);
+
+    division = (mplut, mvolc)
+    for d in division
+        fig = Array{Plots.Plot{Plots.GRBackend}}(undef, length(d))
+        fig_names = string.(d)
+        for i in eachindex(fig)
+            h = plot(
+                framestyle=:box, 
+                grid = false,
+                fontfamily=:Helvetica, 
+                xlims=(40,80),
+                xticks=(40:10:80, string.(40:10:80)),
+                yticks=false
+            )
+
+            # Raw data
+            c, n = bincounts(bulk.SiO2[bulk_cats[d[i]]], 40, 80, 80)
+            n = float(n) ./ nansum(float(n) .* step(c))
+            Plots.plot!(c, n, 
+                seriestype=:bar, barwidths=0.5,
+                color=colors[d[i]], linecolor=:match, alpha=0.25, label=""
+            )
+
+            # Kernel density estimate 
+            u = kde(bulk.SiO2[bulk_cats[d[i]] .& here])
+            Plots.plot!(u.x, u.density, 
+                seriestype=:path, linewidth=4,
+                color=colors[d[i]], linecolor=colors[d[i]], label=""
+            )
+
+            # Final formatting
+            Plots.ylims!(0, round(maximum([n; u.density]), digits=2)+0.01)
+            npoints = count(bulk_cats[d[i]])
+            Plots.annotate!(((0.03, 0.97), (fig_names[i] * "\nn = $npoints", 18, :left, :top)))
+            fig[i] = h
+        end
+        ncols = ceil(Int, length(d)/3)
+        h = Plots.plot(fig..., layout = (3,ncols), size=(600*ncols, 400*3), 
+            title="Bulk Geochemical Data [$geochem_fid]"
+        )
+        display(h)
+    end
+
+
+## --- Silica distribution in volc / plut rocks if we exclude undifferentiated samples 
+    # For igneous rocks, plot volcanic / plutonic silica distribution, where the silica
+    # distribution is made from the unmatched sample distribution weighted by the relative
+    # abundance of each rock class
+
+    # Get proportional distribution on Earth's surface
+    typelist, minorsed, minorvolc, minorplut, minorign = get_rock_class();
+    include_minor!(macro_cats)
+    include_minor!(bulk_cats)
+    here = @. !isnan(mbulk.SiO2);
+
+    ptype = (
+        sed = float.([count(macro_cats[i]) for i in minorsed]),
+        volc = float.([count(macro_cats[i]) for i in minorvolc]),
+        plut = float.([count(macro_cats[i]) for i in minorplut]),
+        ign = float.([count(macro_cats[i]) for i in minorign]),
+    )
+    ptype.sed ./= nansum(ptype.sed)
+    ptype.volc ./= nansum(ptype.volc)
+    ptype.plut ./= nansum(ptype.plut)
+    ptype.ign ./= nansum(ptype.ign)
+
+    # Make distributions
+    minorclasses = (volc=minorvolc, plut=minorplut)
+    for class in (:volc, :plut)
+        dist = zeros(80)
+        for i in eachindex(minorclasses[class])
+            c, n = bincounts(bulk.SiO2[bulk_cats[minorclasses[class][i]]], 40, 80, 80)
+            n = float(n) ./ nansum(float(n) .* step(c))
+            dist .+= (n*ptype[class][i])
+        end
+
+        # Get matched KDE 
+        u = kde(mbulk.SiO2[macro_cats[class] .& here])
+
+        # Assemble plot
+        h = plot(
+            framestyle=:box, 
+            grid = false,
+            fontfamily=:Helvetica, 
+            xlims=(40,80), ylims=(0, maximum(dist)+0.01),
+            xticks=(40:10:80, string.(40:10:80)),
+            yticks=false,
+            xlabel="SiO₂ [wt.%]", ylabel="Relative Abundance",
+            title="$class samples [$geochem_fid]",
+            left_margin=(20,:px)
+        )
+        Plots.plot!(h, cntr(40:0.5:80), dist, 
+            seriestype=:bar, barwidths=0.5,
+            color=colors[class], linecolor=:match, alpha=0.5, label="Weighted Bulk"
+        )
+        Plots.plot!(h, u.x, u.density, 
+            seriestype=:path, linewidth=4,
+            color=colors[class], linecolor=colors[class], label="Matched KDE"
+        )
+        display(h)
+    end
+
+
+## --- OK so as above, but if we resample the distributions for each rock type.
+    # The above code assumes that the silica distribution of each rock class in the
+    # geochemical dataset is representative of the "true" silica distribution. 
+    # 
+    # To avoid making that assumption, what if we spatially resample (NOT temporal, after
+    # Keller et al., 2015) each constituent sub-class before getting its distribution and 
+    # computing the total volcanic / plutonic distribution
+
+    # Set up 
+    include_minor!(macro_cats)
+    include_minor!(bulk_cats)
+    here = @. !isnan(mbulk.SiO2);
+
+    nsims = Int(1e7)
+    SiO₂_error = 1.0
+
+    # Proportional distribution of igneous rock types on Earth's surface
+    typelist, minorsed, minorvolc, minorplut, minorign = get_rock_class();
+    ptype = (
+        volc = float.([count(macro_cats[i]) for i in minorvolc]),
+        plut = float.([count(macro_cats[i]) for i in minorplut]),
+        ign = float.([count(macro_cats[i]) for i in minorign]),
+    )
+    ptype.volc ./= nansum(ptype.volc)
+    ptype.plut ./= nansum(ptype.plut)
+    ptype.ign ./= nansum(ptype.ign)
+
+    # Make plots
+    minorclasses = (volc=minorvolc, plut=minorplut)
+    for class in (:volc, :plut)
+        # Compute distributions
+        dist = zeros(80)
+        for i in eachindex(minorclasses[class])
+            # Get samples in this class
+            s = bulk_cats[minorclasses[class][i]]
+            
+            # Resample
+            k = invweight_location(bulk.Latitude[s], bulk.Longitude[s])
+            p = 1.0 ./ ((k .* nanmedian(5.0 ./ k)) .+ 1.0)
+            data = bulk.SiO2[s]
+            uncertainty = fill(SiO₂_error, length(data))
+            simout = bsresample(data, uncertainty, nsims, p)
+
+            # Get distribution of the simulation and add to the total distribution
+            c, n = bincounts(simout, 40, 80, 80)
+            n = float(n) ./ nansum(float(n) .* step(c))
+            dist .+= (n*ptype[class][i])
+        end
+
+        # Compare resampled distribution of the entire class 
+        k = invweight_location(bulk.Latitude[bulk_cats[class]], bulk.Longitude[bulk_cats[class]])
+        p = 1.0 ./ ((k .* nanmedian(5.0 ./ k)) .+ 1.0)
+        data = bulk.SiO2[bulk_cats[class]]
+        uncertainty = fill(SiO₂_error, length(data))
+        simout = bsresample(data, uncertainty, nsims, p)
+        c₁, n₁ = bincounts(simout, 40, 80, 80)
+        n₁ = float(n₁) ./ nansum(float(n₁) .* step(c₁))
+
+        # Get matched KDE 
+        u = kde(mbulk.SiO2[macro_cats[class] .& here])
+
+        # Assemble plot
+        h = plot(
+            framestyle=:box, 
+            grid = false,
+            fontfamily=:Helvetica, 
+            xlims=(40,80), ylims=(0, maximum(dist)+0.01),
+            xticks=(40:10:80, string.(40:10:80)),
+            yticks=false,
+            xlabel="SiO₂ [wt.%]", ylabel="Relative Abundance",
+            title="$class samples [$geochem_fid]",
+            left_margin=(20,:px)
+        )
+        Plots.plot!(h, cntr(40:0.5:80), dist, 
+            seriestype=:bar, barwidths=0.5,
+            color=colors[class], linecolor=:match, alpha=0.5, label="Weighted Bulk"
+        )
+        Plots.plot!(h, c₁, n₁, 
+            seriestype=:path, linewidth=4, linestyle=:dot,
+            color=:grey, linecolor=:grey, label="Resampled $class"
+        )
+        Plots.plot!(h, u.x, u.density, 
+            seriestype=:path, linewidth=4,
+            color=colors[class], linecolor=colors[class], label="Matched KDE"
+        )
+        display(h)
+    end
+
 
 ## --- End of file

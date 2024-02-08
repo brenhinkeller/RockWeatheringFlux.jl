b80dbe7c8305e1dfabef5f1114e7a488bec984e6
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2024-02-07 22:30:33 -0500

    Fix columns on output file

--- src/UpperCrustComposition.jl
+++ src/UpperCrustComposition.jl
@@ -2,11 +2,12 @@
     # Packages
     using RockWeatheringFlux
     using DelimitedFiles, HDF5
+    using StatsBase
 
 
 ## --- Load data for the matched EarthChem samples
     # Indices and classes of matched samples
-    fid = readdlm("$matchedbulk_io")
+    fid = readdlm(matchedbulk_io)
     bulkidx = Int.(vec(fid[:,1]))
     t = @. bulkidx != 0
 
@@ -26,6 +27,12 @@
     allelements = [majors; minors]
 
 
+## --- Figure out how many geochemical samples explain 90% of the matches 
+    c = countmap(mbulk.Sample_ID)
+    npoints = count(>(percentile(values(c), 90)), values(c))
+    # npoints = count(>(percentile(values(c), 95)), values(c))
+
+
 ## --- Compute and export composition of exposed crust!
     # Some elements just aren't measured, but a good whole rock geochemistry should
     # measure the major elements. If it's a NaN, it's probably just not there fr fr
@@ -43,7 +50,7 @@
     # Save to a file 
     result = Array{Float64}(undef, (length(allelements), length(class)))
     rows = string.(allelements)
-    cols = hcat("", reshape(string.(collect(keys(class))), 1, :))
+    cols = hcat("element", reshape(string.(collect(keys(class))), 1, :))
     
     for i in eachindex(keys(class))
         result[:,i] .= [nanmean(mbulk[j][class[i]]) for j in allelements]

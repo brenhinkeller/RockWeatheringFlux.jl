1b94f46137330e6d5e5b43b8f9252bc59f053eba
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2024-10-09 23:21:06 -0400

    Update end to end run

--- run_endtoend.sh
+++ run_endtoend.sh
@@ -5,30 +5,27 @@
 # Fail if any command fails
 set -e
 
-## --- Generate data files.
-    julia --project="Project.toml" src/ParseMacrostrat.jl   # Lithologic data
-    julia --project="Project.toml" src/ScreenCombined.jl    # Filtered geochemical data
-    julia --project="Project.toml" src/SRTMSlope.jl         # Slope
+## --- Ready, set... 
+    # Get lithologic data from Macrostrat API
+    julia --project="Project.toml" src/ParseMacrostrat.jl
+
+    # Filter geochemical data
+    # julia --project="Project.toml" src/ScreenCombined.jl
+
+    # Calculate slope from SRTM15+
+    # julia --project="Project.toml" src/SRTMSlope.jl
+
+    # Construct slope / erosion model (note model is hardcoded in src/utilities/Slope.jl)
+    # julia --project="Project.toml" src/ModelSlope.jl
 
 
-## --- Construct slope / erosion model (note model is hardcoded in src/utilities/Slope.jl)
-    julia --project="Project.toml" src/ModelSlope.jl
-
-
-## --- Match lithology and geochemistry
+## --- ... go!
     julia --project="Project.toml" src/SampleMatch.jl       
 
-
-## --- Results
-    # Mass of eroded material: undifferentiated, by element, and by lithologic class
-    # Fractional contribution of each lithologic class to total eroded material
-    # Composition of eroded material by lithologic class
-    # Surficial abundance vs. contribution to total eroded material by lithologic class
-    # Mapped surficial abundance of each lithologic class
+    # Eroded material and mapped surfical abundance 
     julia --project="Project.toml" src/CalculateFlux.jl
 
     # Composition of exposed continental crust
-    # Expected surficial distribution of major lithologic classes
     julia --project="Project.toml" src/UpperCrustComposition.jl     
 
 
